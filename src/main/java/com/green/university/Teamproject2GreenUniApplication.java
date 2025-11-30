7840447843be4055c066bf18ec185d8098aa2b3a
yyrrcc <music_v@naver.com>
2025-12-01 00:07:36 +0900

    Initial commit

---
+++ src/main/java/com/green/university/Teamproject2GreenUniApplication.java
@@ -0,0 +1,13 @@
+package com.green.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teamproject2GreenUniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Teamproject2GreenUniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/university/Teamproject2GreenUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teamproject2GreenUniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
