98a727fb861bf339bb4eb710a44e77eecf887285
kmiiyaaa <ckm1533@naver.com>
2025-12-03 17:53:14 +0900

    import 추가

--- src/main/java/com/green/university/controller/BreakAppController.java
+++ src/main/java/com/green/university/controller/BreakAppController.java
@@ -12,12 +12,10 @@ import com.green.university.utils.Define;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
