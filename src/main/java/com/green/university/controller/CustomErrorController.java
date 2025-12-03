0f25cce08b51460442a54313e0179018e68a3925
khyuna0 <khohyuna0102@gmail.com>
2025-12-03 17:48:26 +0900

    CustomErrorController 수정

--- src/main/java/com/green/university/controller/CustomErrorController.java
+++ src/main/java/com/green/university/controller/CustomErrorController.java
@@ -1,16 +1,22 @@
 package com.green.university.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/error")
 public class CustomErrorController {
 
-	@GetMapping("/")
-	public ResponseEntity<?> handleError() {
-		return "/error/errorPage";
-	}
-
+    @GetMapping
+    public ResponseEntity<?> handleError() {
+        return ResponseEntity.status(404).body(Map.of(
+                "error", true,
+                "message", "요청하신 페이지를 찾을 수 없습니다."
+        ));
+    }
 }
