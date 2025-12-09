808fca1e991228e6193dd8e8b9f6dd507b02b93e
kmiiyaaa <ckm1533@naver.com>
2025-12-09 17:38:20 +0900

    handelr import 삭제

--- src/main/java/com/green/university/config/WebMvcConfig.java
+++ src/main/java/com/green/university/config/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package com.green.university.config;
 
-import com.green.university.handler.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
