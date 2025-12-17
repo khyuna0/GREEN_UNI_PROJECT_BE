7143c8c221ed3b6a9365de4aceec3d44c411c718
khyuna0 <khohyuna0102@gmail.com>
2025-12-17 17:56:19 +0900

    성적 확정 상태에서 수정 시 오류 띄우게 변경

--- src/main/java/com/green/university/domain/professor/service/ProfessorService.java
+++ src/main/java/com/green/university/domain/professor/service/ProfessorService.java
@@ -148,6 +148,8 @@ public class ProfessorService {
     @Transactional
     public void updateGrade(Long subjectId, Long studentId, UpdateStudentGradeFormDto dto) {
 
+
+
         // 1. StuSub 찾기
         StuSub stuSub = stuSubRepository
                 .findByStudent_IdAndSubject_Id(studentId, subjectId)
@@ -156,6 +158,10 @@ public class ProfessorService {
         // 2. StuSubDetail 찾기
         StuSubDetail detail = stuSubDetailRepository.findByStuSub(stuSub)
                 .orElseThrow(() -> new RuntimeException("출결 정보 없음"));
+
+        if(detail.isFinalized()) {
+            throw new CustomRestfullException("이미 확정된 성적은 수정할 수 없습니다.", HttpStatus.BAD_REQUEST);
+        }
 
         // 3. 해당 강의 듣는 학생 수 계산 (상대평가 계산 용 / 수강 인원이 10명 이하면 절대평가)
         int numOfStudent = subjectRepository.findNumOfStudentById(subjectId);
