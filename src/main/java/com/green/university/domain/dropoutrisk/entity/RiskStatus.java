1f5ba1932ba10470f10797eb2a2835d5001853f8
kmiiyaaa <ckm1533@naver.com>
2025-12-19 13:14:38 +0900

    상담관련 전체적으로 수정중

--- src/main/java/com/green/university/domain/dropoutrisk/entity/RiskStatus.java
+++ src/main/java/com/green/university/domain/dropoutrisk/entity/RiskStatus.java
@@ -3,6 +3,6 @@ package com.green.university.domain.dropoutrisk.entity;
 // 처리 상태
 public enum RiskStatus {
     DETECTED,    // 감지됨 (학생/교수 알림 띄움)
-    CONSULT_REQ, // 상담 예약됨
-    RESOLVED     // 상담 완료, 해결됨
+    CONSULT_REQ, // 상담 예약 확정
+    RESOLVED     // 상담 끝
 }
