c76eb3adf7ded967951757f45dc00e28371b230e
khyuna0 <khohyuna0102@gmail.com>
2025-12-20 00:30:20 +0900

    배포 테스트용 주석 추가

--- src/main/java/com/green/university/global/config/SecurityConfig.java
+++ src/main/java/com/green/university/global/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
-
+    //
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
