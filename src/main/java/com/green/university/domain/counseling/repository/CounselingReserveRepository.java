c8be9ec1dfc8b7e1ed61fe990ad8ab656beeec4d
yyrrcc <music_v@naver.com>
2025-12-24 11:30:24 +0900

    머지 후 생긴 에러 수정

--- src/main/java/com/green/university/domain/counseling/repository/CounselingReserveRepository.java
+++ src/main/java/com/green/university/domain/counseling/repository/CounselingReserveRepository.java
@@ -102,6 +102,5 @@ public interface CounselingReserveRepository extends JpaRepository<CounselingRes
     CounselingReserve findByRoomCodeAndApprovalState(String roomCode, ApprovalState approvalState);
 
     // 로그인한 유저 + Requester 따라 상담 내역 조회
-    List<CounselingReserve> findByStudent_Id(Long studentId);
     List<CounselingReserve> findBySubject_IdIn(List<Long> subjectIds);
 }
